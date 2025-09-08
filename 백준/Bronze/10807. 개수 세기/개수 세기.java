9ca08eb4af107975e9c83818024f8c25efa75027
yerin028 <165865881+yerin028@users.noreply.github.com>
2025-09-08 19:06:25 +0900

    [Bronze V] Title: 개수 세기, Time: 184 ms, Memory: 17820 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,32 +1,31 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-import static java.lang.System.in;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int array[] = new int[N];
+        int[] num_list = new int[N];
 
-        for (int i=0; i<N; i++) {
-            array[i] = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            num_list[i] = sc.nextInt();
         }
 
-        int K = sc.nextInt();
-        int count = 0;
+        int k = sc.nextInt();
+        int answer = 0;
 
-        for (int i=0; i<N; i++) {
-            if (array[i] == K) {
-                count++;
+        for (int i = 0; i < N; i++) {
+            if (num_list[i] == k) {
+                answer++;
             }
         }
-
-        System.out.println(count);
-
+        System.out.println(answer);
         sc.close();
     }
-
 
 }
