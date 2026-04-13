cb70497e2ab5c7bcc546b2bff4b26662c5cc5277
yerin028 <165865881+yerin028@users.noreply.github.com>
2026-04-13 16:55:07 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 172 ms, Memory: 17836 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a == 0 && b == 0 && c == 0){
+                break;
+            }
+
+            int[] arr = {a, b, c};
+            Arrays.sort(arr);
+
+            if (arr[2] >= arr[0] + arr[1]) {
+                System.out.println("Invalid");
+            } else if (a == b && b == c) {
+                System.out.println("Equilateral");
+            } else if (a == b || b == c || a == c) {
+                System.out.println("Isosceles");
+            } else {
+                System.out.println("Scalene");
+            }
+        }
+    }
+}
