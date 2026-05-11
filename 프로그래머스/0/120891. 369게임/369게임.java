3a5759f8dd13b0402e1610cdce3884dac1ae6372
yerin028 <165865881+yerin028@users.noreply.github.com>
2026-05-11 23:14:13 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+
+        while (order > 0) {
+            int digit = order % 10; 
+            
+            
+            if (digit == 3 || digit == 6 || digit == 9) {
+                answer++;
+            }
+            
+            order /= 10; 
+        }
+
+        return answer;
+    }
+}
